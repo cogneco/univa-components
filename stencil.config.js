4596ac57863e883b633982a0e09ebd4f549deb98
Simon Mika <simon@mika.se>
2018-05-16 08:13:38 +0200

    Added and fixed to comply with improved configuration.

--- stencil.config.js
+++ stencil.config.js
@@ -1,17 +1,17 @@
 exports.config = {
-  namespace: 'mycomponent',
-  outputTargets:[
-    { 
-      type: 'dist' 
-    },
-    { 
-      type: 'www',
-      serviceWorker: false
-    }
-  ]
+	namespace: 'mycomponent',
+	outputTargets:[
+		{
+			type: 'dist'
+		},
+		{
+			type: 'www',
+			serviceWorker: false
+		}
+	]
 };
 
 exports.devServer = {
-  root: 'www',
-  watchGlob: '**/**'
+	root: 'www',
+	watchGlob: '**/**'
 }
